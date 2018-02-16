7f2a4c27ef09ebeb98f2adf80d81152fd889cdd6
Max Filippov <jcmvbkbc@gmail.com>
2018-02-16 10:20:36 -0800

    add ssh-invert.c
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ ssh-invert.c
@@ -0,0 +1,33 @@
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+        char c[4096];
+        fd_set readfds;
+
+        FD_ZERO(&readfds);
+        for (;;) {
+                ssize_t rd = read(STDIN_FILENO, c, sizeof(c));
+                if (rd < 0) {
+                        FD_SET(STDIN_FILENO, &readfds);
+                        select(STDIN_FILENO + 1, &readfds, NULL, NULL, NULL);
+                } else if (rd == 0) {
+                        return 0;
+                } else {
+                        int i;
+                        for (i = 0; i < rd; ++i)
+                                c[i] ^= 0x5a;
+                        for (i = 0; i < rd; ) {
+                                ssize_t wr = write(STDOUT_FILENO, c + i, rd - i);
+                                if (wr == 0)
+                                        return 0;
+                                else if (wr > 0)
+                                        i += wr;
+                        }
+                }
+        }
+        return 0;
+}
